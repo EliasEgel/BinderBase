6afa811188684d9bbd4170771eee28d160f1ebaf
EliasEgel <elias.egelrud@gmail.com>
2025-11-04 13:30:28 +0100

    fix: crossorigin

--- backend/src/main/java/org/example/backend/controller/ChatController.java
+++ backend/src/main/java/org/example/backend/controller/ChatController.java
@@ -11,14 +11,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/chat")
 public class ChatController {
